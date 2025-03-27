6aef69b99ed2ca6bceba12864a24d9643bd80ad5
mrnigesh <mrnigesh2006@gmail.com>
2025-03-27 11:14:44 +0530

    Init

---
+++ frontend/src/contractConfig.js
@@ -0,0 +1,59 @@
+import { BrowserProvider, Contract } from 'ethers';
+
+const CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const CONTRACT_ABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_name",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_email",
+        "type": "string"
+      }
+    ],
+    "name": "setIdentity",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_user",
+        "type": "address"
+      }
+    ],
+    "name": "getIdentity",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      },
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+
+export const getContract = () => {
+  if (!window.ethereum) throw new Error("MetaMask is not installed");
+  const provider = new BrowserProvider(window.ethereum);
+  const signer = provider.getSigner();
+  return new Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
+};
